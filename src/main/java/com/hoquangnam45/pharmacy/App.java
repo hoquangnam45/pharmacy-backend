7cc45d5cf743325b8d3cf9b734b9d2ef2aaf1d35
hoquangnam45 <hoquangnam45@gmail.com>
2024-01-25 13:07:12 +0700

    in-progress

--- src/main/java/com/hoquangnam45/pharmacy/App.java
+++ src/main/java/com/hoquangnam45/pharmacy/App.java
@@ -9,6 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 // TODO: Add rating ->
 // TODO: Add job for confirm email send out upon registered
 // TODO: Add chat + support if needed
+// TODO: Integrate with payment transaction processing service like momo / napas / bank / credit card
+// TODO: Start working on FE
+// TODO: Setup deployment / pipeline / config for production
 @SpringBootApplication
 @EnableScheduling
 public class App {
